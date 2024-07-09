fb0595671dca33ba0072c3ecea22135051ed3003
fredev <federico@aternum.dev>
2024-07-09 16:32:03 +0200

    feat: init project

---
+++ apps/prices/src/db/schema.ts
@@ -0,0 +1,9 @@
+import { pgTable, numeric, varchar } from 'drizzle-orm/pg-core';
+
+
+export const prices = pgTable('prices', {
+  symbol: varchar('symbol').primaryKey().notNull(),
+  price: numeric('price').notNull(),
+  volume: numeric('volume').notNull(),
+  marketCap: numeric('marketCap').notNull(),
+});
