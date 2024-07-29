4705e5141b8ebb9f31931dee8ab9682513c19bce
fredev <federico@aternum.dev>
2024-07-29 10:59:32 +0200

    feat: remove prices app

--- apps/prices/drizzle/0000_natural_morg.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS "prices" (
-	"symbol" varchar PRIMARY KEY NOT NULL,
-	"price" numeric NOT NULL,
-	"volume" numeric NOT NULL,
-	"marketCap" numeric NOT NULL
-);
--- apps/prices/src/db/schema.ts
+++
@@ -1,8 +0,0 @@
-import { pgTable, numeric, varchar } from 'drizzle-orm/pg-core';
-
-export const prices = pgTable('prices', {
-  symbol: varchar('symbol').primaryKey().notNull(),
-  price: numeric('price').notNull(),
-  volume: numeric('volume').notNull(),
-  marketCap: numeric('marketCap').notNull(),
-});
